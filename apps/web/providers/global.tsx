ffcf22eaeee3569dd2d3c934dced31144972c269
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2026-06-20 14:46:03 +0530

    system-theme-detection

--- apps/web/providers/global.tsx
+++ apps/web/providers/global.tsx
@@ -27,7 +27,6 @@ export const GlobalProviders: React.FC<{ children: React.ReactNode }> = ({ child
     <QueryClientProvider client={queryClient}>
       <NextThemesProvider
         attribute="class"
-        defaultTheme="light"
         enableSystem
         disableTransitionOnChange
       >
