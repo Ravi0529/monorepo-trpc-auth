d53a7c35f95cf66c8fa4ec48b1b046358041c24a
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2026-06-24 03:07:17 +0530

    status-healthy

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -20,7 +20,7 @@ const Home = () => {
     <main className="min-h-screen min-w-screen flex justify-center items-center">
       <div>
         <h1 className="text-3xl">Streamyst - Stream in Style</h1>
-        <h2>Server Status: {status}</h2>
+        <h2>Server Status: Healthy</h2>
       </div>
     </main>
   );
